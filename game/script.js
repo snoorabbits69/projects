69b9d45e05fecee59c4c6935e8bf8c012f6c554f
Roshan Adhikari <86873831+snoorabbits69@users.noreply.github.com>
2023-04-25 06:22:09 +0545

    update

--- game/script.js
+++ game/script.js
@@ -1,221 +1,230 @@
-const images={
-  hills:"../image/hills.png",
-  background:"../image/background.png",
-  platformsmall:"../image/platformsmalll.png"
+const images = {
+  hills: "../image/hills.png",
+  background: "../image/background.png",
+  platformsmall: "../image/platformsmalll.png"
 }
+let death = 0;
 console.log(images.background);
 const image = new Image();
 image.src = '../image/platform.png';
 
 
-image.addEventListener('load',()=>{
- 
+image.addEventListener('load', () => {
+
   console.log(image.width);
   console.log(image.height);
 
 
-const canvas =document.querySelector('canvas');
+  const canvas = document.querySelector('canvas');
 
 
-let movement=0;
-  canvas.width=1024;
-  canvas.height=576;
-  let c=canvas.getContext('2d');
-  const gravity=0.5;
-class Player{
-    constructor()
-    {
-        this.position={
-            x:100,
-            y:100
-        }
-        this.width=30;
-        this.height=30;
-        this.velocity={
-            x:0,
-            y:0
-        }
+  let movement = 0;
+  canvas.width = 1024;
+  canvas.height = 576;
+  let c = canvas.getContext('2d');
+  const gravity = 0.5;
+
+  class Player {
+    constructor() {
+      this.position = {
+        x: 100,
+        y: 100
+      }
+      this.width = 30;
+      this.height = 30;
+      this.velocity = {
+        x: 0,
+        y: 0
+      }
     }
-    draw(){
-        c.fillStyle="blue";
-        c.fillRect(this.position.x,this.position.y,this.width,this.height);
+    draw() {
+      c.fillStyle = "blue";
+      c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
-    update(){
+    update() {
 
-        this.draw();
-        this.position.y+=this.velocity.y;
-        this.position.x+=this.velocity.x;
-        if(this.position.y+this.velocity.y+this.height<canvas.height){
-        this.velocity.y+=gravity;
-        }
-        
+      this.draw();
+      this.position.y += this.velocity.y;
+      this.position.x += this.velocity.x;
+      if (this.position.y + this.velocity.y + this.height < canvas.height) {
+        this.velocity.y += gravity;
+      }
+
     }
-}
+  }
 
-class Platform{
-    constructor(x,y,image){
-this.position={
-    x,y
-}
-this.image=image;
-this.width=this.image.width;
-this.height=image.height;
+  class Platform {
+    constructor(x, y, image) {
+      this.position = {
+        x, y
+      }
+      this.image = image;
+      this.width = this.image.width;
+      this.height = image.height;
     }
-    draw(){
-        c.drawImage(this.image,this.position.x,this.position.y)
+    draw() {
+      c.drawImage(this.image, this.position.x, this.position.y)
     }
-}
+  }
 
-const player=new Player();
+  const player = new Player();
 
-const platforms=[new Platform(10,460,image),new Platform(image.width,460,image),new Platform(image.width*2.3,460,image)];
-console.log(image.width);
+  const platforms = [new Platform(10, 460, image), new Platform(image.width, 460, image), new Platform(image.width * 2.3, 460, image)];
+  console.log(image.width);
 
-function createimg(imagesrc){
-const img=new Image();
-img.src=imagesrc;
-return img;
-}
+  function createimg(imagesrc) {
+    const img = new Image();
+    img.src = imagesrc;
+    return img;
+  }
 
-  class genericobjects{
-    constructor(x,y,image){
-this.position={
-    x,y
-}
-this.image=image;
-this.width=image.width;
-this.height=image.height;
+  class genericobjects {
+    constructor(x, y, image) {
+      this.position = {
+        x, y
+      }
+      this.image = image;
+      this.width = image.width;
+      this.height = image.height;
     }
-    draw(){
-        c.drawImage(this.image,this.position.x,this.position.y)
+    draw() {
+      c.drawImage(this.image, this.position.x, this.position.y)
     }
-}
-const genericobj=[new genericobjects(0,0,createimg(images.background)),new genericobjects(-1,-1,createimg(images.hills))]
-const keys={
-right:{
-pressed:false
-},left:{
-    pressed:false
-}
-}
+  }
+  const genericobj = [new genericobjects(0, 0, createimg(images.background)), new genericobjects(-1, -1, createimg(images.hills))]
+  const keys = {
+    right: {
+      pressed: false
+    }, left: {
+      pressed: false
+    }
+  }
 
-function animate(){
+  function animate() {
     requestAnimationFrame(animate);
-    c.fillStyle="white";
-    c.fillRect(0,0,canvas.width,canvas.height);
-    genericobj.forEach(generic=>{generic.draw();})
-  player.update();
+    c.fillStyle = "white";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    genericobj.forEach(generic => { generic.draw(); })
+    player.update();
 
-platforms.forEach(platform=>{
-  platform.draw();
-}
-)
-  if(keys.right.pressed && player.position.x<400){
-    player.velocity.x=5;
-    
-  }
-  else if(keys.left.pressed && player.position.x>100){
-    player.velocity.x=-5;
-  }
-  else{
-    player.velocity.x=0;
-    if(keys.right.pressed){
-        platforms.forEach(platform=>{
-            
- platform.position.x-=5;
- 
+    platforms.forEach(platform => {
+      platform.draw();
+    }
+    )
+    if (keys.right.pressed && player.position.x < 400) {
+      player.velocity.x = 5;
+
+    }
+    else if (keys.left.pressed && player.position.x > 100) {
+      player.velocity.x = -5;
+    }
+    else {
+      player.velocity.x = 0;
+      if (keys.right.pressed) {
+        platforms.forEach(platform => {
+
+          platform.position.x -= 5;
+
 
         })
-        genericobj.forEach(generic=>{generic.position.x-=3;})
-        movement+=5;
-    }
-    else if(keys.left.pressed){
-        platforms.forEach(platform=>{
-        platform.position.x+=5;
+        genericobj.forEach(generic => { generic.position.x -= 3; })
+        movement += 5;
+      }
+      else if (keys.left.pressed) {
+        platforms.forEach(platform => {
+          platform.position.x += 5;
         })
-        genericobj.forEach(generic=>{generic.position.x+=3;})
-        movement-=5;
+        genericobj.forEach(generic => { generic.position.x += 3; })
+        movement -= 5;
+      }
+      if (movement > 4000) {
+        console.log("You did it");
+      }
+      if (player.position.y > canvas.height) {
+        death = 400;
+        console.log(death);
+      }
     }
-if(movement>4000){
-    console.log("You did it");
-}
-if(player.position.y>canvas.height){
-  console.log("400");
-}
+    platforms.forEach(platform => {
+      if ((player.height + player.position.y) <= platform.position.y
+        && (player.height + player.position.y + player.velocity.y) >= platform.position.y
+        && (player.position.x + player.velocity.x) >= platform.position.x
+        && player.position.x < (platform.position.x + platform.width)) {
+        player.velocity.y = 0;
+      }
+    })
   }
-  platforms.forEach(platform=>{
-    if((player.height+player.position.y)<=platform.position.y 
-        &&(player.height+player.position.y+player.velocity.y)>=platform.position.y
-        && (player.position.x+player.velocity.x)>=platform.position.x
-        &&player.position.x<(platform.position.x+platform.width)){
-     player.velocity.y=0;
-    }
-})
-}
-animate();
-addEventListener('keydown',({keyCode})=>{
-// console.log(keyCode);
-
-switch (keyCode) {
-    case 65:
-    case 37:
-      // console.log("left");
-      player.velocity.x -= 1;
-      keys.left.pressed = true;
-      break;
-    case 68:
-    case 39:
-      // console.log("right");
-      player.velocity.x += 1;
-      keys.right.pressed = true;
-      break;
-    case 87:
-    case 38:
-    case 32:
-      // console.log("up");
-      player.velocity.y -= 10;
-      break;
-    case 83:
-    case 40:
-      // console.log("down");
-      player.velocity.y += 10;
-      break;
-    default:
-      break;
-  }
-  
-                                                       
-
-})
-addEventListener('keyup',({keyCode})=>{
+  animate();
+  addEventListener('keydown', ({ keyCode }) => {
+    // console.log(keyCode);
 
     switch (keyCode) {
-        case 65:
-        case 37:
-          // console.log("left");
-          player.velocity.x = 0;
-          keys.left.pressed = false;
-          break;
-        case 68:
-        case 39:
-          // console.log("right");
-          player.velocity.x = 0;
-          keys.right.pressed = false;
-          break;
-        case 87:
-        case 38:
-        case 32:
-          // console.log("up");
-          player.velocity.y = 0;
-          break;
-        case 83:
-        case 40:
-          // console.log("down");
-          player.velocity.y = 0;
-          break;
-        default:
-          break;
-      }
-        })
-        
-      })
+      case 65:
+      case 37:
+        // console.log("left");
+        player.velocity.x -= 1;
+        keys.left.pressed = true;
+        break;
+      case 68:
+      case 39:
+        // console.log("right");
+        player.velocity.x += 1;
+        keys.right.pressed = true;
+        break;
+      case 87:
+      case 38:
+      case 32:
+        // console.log("up");
+        if (death == 0) {
+          player.velocity.y -= 10;
+        }
+        break;
+
+
+      case 83:
+      case 40:
+        // console.log("down");
+
+        player.velocity.y += 10;
+
+        break;
+
+      default:
+        break;
+    }
+
+
+
+  })
+  addEventListener('keyup', ({ keyCode }) => {
+
+    switch (keyCode) {
+      case 65:
+      case 37:
+        // console.log("left");
+        player.velocity.x = 0;
+        keys.left.pressed = false;
+        break;
+      case 68:
+      case 39:
+        // console.log("right");
+        player.velocity.x = 0;
+        keys.right.pressed = false;
+        break;
+      case 87:
+      case 38:
+      case 32:
+        // console.log("up");
+        player.velocity.y = 0;
+        break;
+      case 83:
+      case 40:
+        // console.log("down");
+        player.velocity.y = 0;
+        break;
+      default:
+        break;
+    }
+  })
+
+})
